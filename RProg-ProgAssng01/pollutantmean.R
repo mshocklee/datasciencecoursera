ae25554b721b95d07aacede9b4ca3d598445b76e
mshocklee <mshocklee@users.noreply.github.com>
2015-02-14 01:41:01 -0600

    Complete pollutantmean.R

--- RProg-ProgAssng01/pollutantmean.R
+++ RProg-ProgAssng01/pollutantmean.R
@@ -12,5 +12,15 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
 		
-		
+		pset<-data.frame(x=numeric(0))
+		names(pset) <- pollutant
+		for (i in 1:length(id)) {
+			while(nchar(id[i])<3) {
+				id[i] <- paste("0",id[i],sep="")
+			}
+			monData <- read.csv(file=paste0(directory,"/",id[i],".csv"))
+			pset <- rbind(pset,subset(monData, select=pollutant))
+		}
+		pmean <- round(as.numeric(colMeans(pset,na.rm=TRUE)),digits=3)
+		return(pmean)
 }
