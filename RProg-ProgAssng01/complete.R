65caf9fa0fd04e4a4bf9715b501b2cbf2d11a50c
mshocklee <mshocklee@users.noreply.github.com>
2015-02-15 08:19:46 -0600

    Mod complete.R

--- RProg-ProgAssng01/complete.R
+++ RProg-ProgAssng01/complete.R
@@ -13,12 +13,12 @@ complete <- function(directory, id = 1:332) {
         ## where 'id' is the monitor ID number and 'nobs' is the
         ## number of complete cases
 		
-		files <- list.files(directory)
+		files <- paste0(directory,"/",list.files(directory))
 		file.data <- data.frame()
 		nobs.data <- data.frame("id"= numeric(length=length(id)), "nobs"= numeric(length=length(id)))
 		
 		for(i in 1:length(id)) {
-				file.data <- read.csv(paste0(directory,"/",files[id[i]]))
+				file.data <- read.csv(files[id[i]])
 				nobs.data[i,1:2] <- c(id[i],sum(complete.cases(file.data)))
 		}
 		nobs.data
