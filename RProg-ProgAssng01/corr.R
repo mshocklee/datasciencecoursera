a71195d8b25807d217187f838ff544052028c99f
mshocklee <mshocklee@users.noreply.github.com>
2015-02-15 09:36:36 -0600

    Corrected corr.R

--- RProg-ProgAssng01/corr.R
+++ RProg-ProgAssng01/corr.R
@@ -10,13 +10,14 @@ corr <- function(directory, threshold = 0) {
         ## Return a numeric vector of correlations
 		
 		comp <- subset(complete(directory), nobs > threshold)
-		files <- paste0(directory,"/",list.files(directory)[comp[[1]]])
 		nsCorr <- numeric(0)
+		if(nrow(comp) == 0) return(nsCorr)
+		files <- paste0(directory,"/",list.files(directory)[comp[[1]]])
 		abvThresh <- data.frame()
 		
 		for(i in 1:nrow(comp)) {
 			abvThresh <- read.csv(files[i])
-			nsCorr <- c(nsCorr,cor(abvThresh$nitrate, abvThresh$sulfate,use="complete.obs"))
+			nsCorr <- c(nsCorr,cor(abvThresh$sulfate, abvThresh$nitrate,use="complete.obs"))
 		}
-		nsCorr <- round(nsCorr,digits=5)
+		nsCorr
 }
